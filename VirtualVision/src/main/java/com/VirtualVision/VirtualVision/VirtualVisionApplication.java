989cc8c8552ea96e05749c36c13ca262556c9c66
galbesa <galbesa@alumnat.copernic.cat>
2023-11-15 21:14:01 +0100

    Base Project

---
+++ VirtualVision/src/main/java/com/VirtualVision/VirtualVision/VirtualVisionApplication.java
@@ -0,0 +1,13 @@
+package com.VirtualVision.VirtualVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualVisionApplication.class, args);
+	}
+
+}
---
+++ VirtualVision/src/test/java/com/VirtualVision/VirtualVision/VirtualVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VirtualVision.VirtualVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
