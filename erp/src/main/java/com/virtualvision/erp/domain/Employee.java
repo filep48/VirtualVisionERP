41928fd6b287d5092c9ad7ee7f1e0da9ac02cef9
vpedrola <vpedrola@alumnat.copernic.cat>
2023-12-04 20:34:48 +0100

    commit gerard

---
+++ erp/src/main/java/com/virtualvision/erp/domain/Employee.java
@@ -0,0 +1,64 @@
+package com.virtualvision.erp.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.ToString;
+
+
+@Entity
+@Data
+public class Employee {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String surname;
+
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotEmpty
+    @Column(nullable = false)
+    @ToString.Exclude
+    private String password;
+
+    @Column(name = "dni_nif")
+    private String dniNif;
+
+    @Column(nullable = true)
+    private String phone;
+
+    
+    @Column(name = "email")
+    private String email;
+
+    @Column(nullable = true)
+    private String address;
+
+    @Column(nullable = false)
+    private String role = "employee";
+
+    @Column(nullable = true)
+    private String position;
+
+    @Column(name = "num_ss")
+    private String numSS;
+
+    @Column(nullable = true)
+    private double salary;
+
+    
+}
